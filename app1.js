de3cb22a4991f8ab3e9f996b021a391a16b47236
Dorapis0914 <koyukumae09141@gmail.com>
2025-05-20 21:39:45 +0900

    commit

--- app1.js
+++ app1.js
@@ -1,10 +1,15 @@
 //index.html1ファイルのjs
 function ButtonClick(){
+    function changeImage() {
+        const imgElement = document.getElementById("myImage");
+        imgElement.src = "画像フォルダ/ダウンロード.png";
+        imgElement.style.display = "block";
+    }
     const a = document.getElementById("Text");
     const text = a.value;
     switch (text) {
         case "1":
-            location.replace("https://www.google.com/imgres?q=1%E5%9B%9E%E3%81%BF%E3%81%9F%E3%82%89%E6%AD%BB%E3%81%AC%E7%B5%B5&imgurl=https%3A%2F%2Fi0.wp.com%2Ftoshidensetsu-ikki.com%2Fwp-content%2Fuploads%2Fdageefee.jpg%3Fresize%3D474%252C640&imgrefurl=https%3A%2F%2Ftoshidensetsu-ikki.com%2Fhorror%2Fcurse-picture&docid=34IA2iIQnehhoM&tbnid=zp34Z2fSh2rXQM&vet=12ahUKEwj1w6P54K-NAxV2ka8BHU_PJlEQM3oECF0QAA..i&w=474&h=640&hcb=2&ved=2ahUKEwj1w6P54K-NAxV2ka8BHU_PJlEQM3oECF0QAA")
+            changeImage()
             break;
         case "2":
             alert(2)
