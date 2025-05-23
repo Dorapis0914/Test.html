7e9673dff3342048b2898e870022d9afd3505ee1
Dorapis0914 <koyukumae09141@gmail.com>
2025-05-23 23:48:10 +0900

    commit

--- app1.js
+++ app1.js
@@ -6,21 +6,22 @@ function ButtonClick(){
         const imgElement = document.getElementById("myImage");
         switch (text) {
             case "1":
-                imgElement.src = "https://lh3.googleusercontent.com/Kthiw2B3IDE-8Ef7VSmdEnbkVEFXzoTOw7o4fXqxNk1L9TcYD68pRLxzWTFbKS2Q3ogNS0Pks5Q2UM3HAG1e=s1200-rw";
-                imgElement.style.display = "blcok";
+                imgElement.src = "画像フォルダ/1回見たら死ぬ絵.webp";
+                imgElement.style.display = "block";
                 break;
             case "2":
-                alert("No image")
+                imgElement.src = "画像フォルダ/doraemon.jpg"
+                imgElement.style.display = "block";
                 break;
             case "3":
-                imgElement.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9_jZlioOC4uGOWQyHfZ_ESWAD7YRfJEklng&s";
-                imgElement.style.display = "blcok";
+                imgElement.src = "画像フォルダ/3回みたら死ぬ絵.jpg";
+                imgElement.style.display = "block";
                 break;
             case "4":
                 alert("No image");
                 break;
             case "5":
-                imgElement.src = "https://i0.wp.com/toshidensetsu-ikki.com/wp-content/uploads/dabareefee.jpg?resize=620%2C500";
+                imgElement.src = "画像フォルダ/5回みたら死ぬ絵.webp";
                 imgElement.style.display = "blcok";
                 break;
             default:
