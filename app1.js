535b2cbfe6c0e24dd8bd014b9f96cd5e3f3f0da7
Dorapis0914 <koyukumae09141@gmail.com>
2025-05-24 00:10:56 +0900

    commit

--- app1.js
+++ app1.js
@@ -2,28 +2,29 @@
 function ButtonClick(){
     const a = document.getElementById("Text");
     const text = a.value;
-    function changeImage() {
-        const imgElement = document.getElementById("myImage");
-        switch (text) {
-            case "1":
-                imgElement.src = "画像フォルダ/1回見たら死ぬ絵.webp";
-                break;
-            case "2":
-                imgElement.src = "画像フォルダ/No image.png";
-                break;
-            case "3":
-                imgElement.src = "画像フォルダ/3回みたら死ぬ絵.jpg";
-                break;
-            case "4":
-                imgElement.src = "画像フォルダ/No image.png";
-                break;
-            case "5":
-                imgElement.src = "画像フォルダ/5回みたら死ぬ絵.webp";
-                break;
-            default:
-                break;
-        }
+    const imgElement = document.getElementById("myImage"); // 追加
+    if (text =="") {
+        imgElement.src = ""; // 画像を消す
+    } else {
+            switch (text) {
+                case "1":
+                    imgElement.src = "画像フォルダ/1回見たら死ぬ絵.webp";
+                    break;
+                case "2":
+                    imgElement.src = "画像フォルダ/No image.png";
+                    break;
+                case "3":
+                    imgElement.src = "画像フォルダ/3回みたら死ぬ絵.jpg";
+                    break;
+                case "4":
+                    imgElement.src = "画像フォルダ/No image.png";
+                    break;
+                case "5":
+                    imgElement.src = "画像フォルダ/5回みたら死ぬ絵.webp";
+                    break;
+                default:
+                    break;
+            }
+        a.value = "";
     }
-changeImage();
-a.value = "";
-    }
+}
