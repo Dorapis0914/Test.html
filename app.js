2db9caf919b186b7d9f5ae07f3fd7e72978c9fb4
Dorapis0914 <koyukumae09141@gmail.com>
2025-05-28 20:14:17 +0900

    commit

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-//index.htmlファイルのjs
-function Namebutton() {
-    const text = document.getElementById("sample");
-    const text2 = document.getElementById("sample2");
-    const s = text.value;//.取得した要素に.valueの拡張子をつける
-    const b = text2.value;//"
-    alert(Number(s)+Number(b)); //Numberで数値型にする
+function onclickbutton() {
+    
 }
-
